88ae4e405cf1811d4f13c53baa06a9a8a90c3d50
AmiMappy <mahapatramanu@icloud.com>
2020-10-02 08:45:54 -0500

    Updated dyk.py

--- dyk.py
+++ dyk.py
@@ -1,11 +1,15 @@
 import wikipedia
 import os
 from bs4 import BeautifulSoup
+import sys, os
+
+pathname = os.path.dirname(sys.argv[0])
+fullpath = os.path.abspath(pathname)
 
 mpsrc = "https://en.wikipedia.org/wiki/Main_Page"
-os.system("touch /home/mappy/Desktop/goodMorning/wmpsrc.html")
-os.system("wget " + mpsrc + " -O /home/mappy/Desktop/goodMorning/wmpsrc.html > /dev/null 2>&1")
-file = open("/home/mappy/Desktop/goodMorning/wmpsrc.html", "r")
+os.system("touch " + fullpath + "/wmpsrc.html")
+os.system("wget " + mpsrc + " -O "+ fullpath + "/wmpsrc.html > /dev/null 2>&1")
+file = open(fullpath + "/wmpsrc.html", "r")
 
 soup = BeautifulSoup(file.read(), "html.parser")
 div = soup.find_all(id="mp-dyk")
